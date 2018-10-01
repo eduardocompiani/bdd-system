8fa49c62281414d2f238a6ec608e7b619431492a
Eduardo Compiani <eduardovieiracompiani@gmail.com>
2018-10-01 20:12:30 -0300

    Corrigido erro no formulario de login onde o email nao estava validando corretamente

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@angular/core';
+import {UserInterface} from '../interfaces/user-interface';
+import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  userCollection: AngularFirestoreCollection<UserInterface>;
+  constructor(private afs: AngularFirestore) {
+    this.userCollection = this.afs.collection('users');
+  }
 
-  constructor() { }
+  update(user: UserInterface) {
+    this.userCollection.doc(user.uid).set({
+      'name': user.name,
+      'email': user.email,
+      'uid': user.uid,
+      'roles': {
+        'canManageFolders': false,
+        'canManageScenarios': false,
+        'canManageTeam': false
+      }
+    });
+  }
 
 }
