055190be6d047ed83f650468a5e5fd5332ab10e5
Eduardo Compiani <eduardovieiracompiani@gmail.com>
2018-11-25 00:38:08 -0200

    movido formulario para um componente proprio

--- src/app/components/main/scenario/scenario-create/scenario-create.component.ts
+++ src/app/components/main/scenario/scenario-create/scenario-create.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {ScenarioInterface} from '../../../../interfaces/scenario-interface';
-import {FolderService} from '../../../../services/folder.service';
-import {FolderInterface} from '../../../../interfaces/folder-interface';
-import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-scenario-create',
@@ -11,41 +8,11 @@ import {Observable} from 'rxjs';
 })
 export class ScenarioCreateComponent implements OnInit {
 
-  scenario: ScenarioInterface;
-  folders: Observable<FolderInterface[]>;
-
-  constructor(private folderService: FolderService) { }
-
   ngOnInit() {
-    this.scenario = {
-      description: '',
-      folder: '',
-      cases: [
-        {
-          testGiven: '',
-          testWhen: '',
-          testThen: ''
-        }
-      ]
-    };
-
-    this.folders = this.folderService.getList();
   }
 
-  addCase() {
-    this.scenario.cases.push({
-      testGiven: '',
-      testWhen: '',
-      testThen: ''
-    });
-  }
-
-  save() {
-    console.log(this.scenario);
-  }
-
-  deleteCase(index: number) {
-    this.scenario.cases.splice(index - 1, 1);
+  createScenario(scenario: ScenarioInterface) {
+    console.log(scenario);
   }
 
 }
--- src/app/components/main/scenario/scenario-form/scenario-form.component.ts
+++ src/app/components/main/scenario/scenario-form/scenario-form.component.ts
@@ -1,4 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ScenarioInterface} from '../../../../interfaces/scenario-interface';
+import {FolderService} from '../../../../services/folder.service';
+import {Observable} from 'rxjs';
+import {FolderInterface} from '../../../../interfaces/folder-interface';
 
 @Component({
   selector: 'app-scenario-form',
@@ -7,11 +11,44 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ScenarioFormComponent implements OnInit {
 
+  folders: Observable<FolderInterface[]>;
+  @Input() scenario: ScenarioInterface;
   @Input() editable: boolean = false;
+  @Output() onSave = new EventEmitter();
 
-  constructor() { }
+  constructor(private folderService: FolderService) { }
 
   ngOnInit() {
+    this.folders = this.folderService.getList();
+
+    if (!this.scenario) {
+      this.scenario = {
+        description: '',
+        folder: '',
+        cases: [
+          {
+            testGiven: '',
+            testWhen: '',
+            testThen: ''
+          }
+        ]
+      };
+    }
   }
 
+  addCase() {
+    this.scenario.cases.push({
+      testGiven: '',
+      testWhen: '',
+      testThen: ''
+    });
+  }
+
+  emitSave() {
+    this.onSave.emit(this.scenario);
+  }
+
+  deleteCase(index: number) {
+    this.scenario.cases.splice(index - 1, 1);
+  }
 }
