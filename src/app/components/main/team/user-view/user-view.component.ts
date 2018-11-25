717a11f359f397e1308425fbf6f63a6a74868bdb
Eduardo Compiani <eduardovieiracompiani@gmail.com>
2018-11-25 19:32:50 -0200

    visualizar usuario

--- src/app/components/main/team/user-view/user-view.component.ts
+++ src/app/components/main/team/user-view/user-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UserInterface} from '../../../../interfaces/user-interface';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserService} from '../../../../services/user.service';
 
 @Component({
   selector: 'app-user-view',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserViewComponent implements OnInit {
 
-  constructor() { }
+  user: UserInterface;
+
+  constructor(private route: ActivatedRoute,
+              private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getByOid(this.route.snapshot.params['id']).subscribe(user => {
+      this.user = user as UserInterface;
+    });
   }
 
 }
